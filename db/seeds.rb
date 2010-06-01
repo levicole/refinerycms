546053bbe32e75017effc23923b1410221a76ab6
stevenheidel <steven@livingskyweb.ca>
2010-06-01 14:52:19 -0600

    Deleted non gh-pages stuffs

--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-# Create a default themes directory.
-Rails.root.join("themes").mkdir unless Rails.root.join("themes").directory?
-
-# Refinery settings
-Dir[Rails.root.join('db', 'seeds','*.rb')].each do |file|
-  require file
-end
--- vendor/plugins/resources/config/routes.rb
+++
@@ -1,9 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-
-  map.resources :resources
-
-  map.namespace(:admin, :path_prefix => 'refinery') do |admin|
-    admin.resources :resources, :collection => {:insert => :get}
-  end
-
-end
